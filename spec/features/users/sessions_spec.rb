807a2d4d36b2370c4a99a0701c29f66421c4e75c
Anna Marie Sterling <sterling.am@gmail.com>
2022-10-24 14:57:14 -0600

    Invalid Credentials

--- spec/features/users/sessions_spec.rb
+++ spec/features/users/sessions_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'user login' do
   let!(:user2) { users.second }
   let!(:user3) { users.third }
 
-  it 'logs a user in and out' do
+  it 'logs a user in' do
     visit root_path
 
     expect(page).to have_field(:email)
@@ -21,5 +21,18 @@ RSpec.describe 'user login' do
     expect(page).to have_content("Welcome #{user1.name}")
     expect(page).to_not have_button('Log In')
     expect(page).to_not have_button('Create New User')
+  end
+
+  it 'does not log a user in with invalid credentials' do
+    visit root_path
+
+    fill_in :email, with: "#{user1.email}"
+    fill_in :password, with: 'wrong'
+    click_button 'Log In'
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content('Invalid Credentials')
+    expect(page).to have_button('Log In')
+    expect(page).to have_button('Create New User')
   end
 end
