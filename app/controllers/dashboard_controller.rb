db84229960d3714fd6864df3ca5e0c8ab6fde911
Anna Marie Sterling <sterling.am@gmail.com>
2022-10-24 14:51:47 -0600

    Complete authentication challenge

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,12 @@
 class DashboardController < ApplicationController
   include ActionView::Helpers::TextHelper
-
+  before_action :require_user
   def show
+  end
+
+  private
+
+  def require_user
     @user = User.find(params[:user_id])
   end
 end
---
+++ spec/features/users/sessions_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'user login' do
+  let!(:users) { create_list(:user, 3) }
+  let!(:user1) { users.first }
+  let!(:user2) { users.second }
+  let!(:user3) { users.third }
+
+  it 'logs a user in and out' do
+    visit root_path
+
+    expect(page).to have_field(:email)
+    expect(page).to have_field(:password)
+    expect(page).to have_button('Log In')
+
+    fill_in :email, with: "#{user1.email}"
+    fill_in :password, with: "#{user1.password}"
+    click_button 'Log In'
+
+    expect(current_path).to eq(user_dashboard_path(user1))
+    expect(page).to have_content("Welcome #{user1.name}")
+    expect(page).to_not have_button('Log In')
+    expect(page).to_not have_button('Create New User')
+  end
+end
